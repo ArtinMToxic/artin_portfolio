33fe9083469a1b62de125a7e045e903ba89f6f69
Aritntoxic <artintoxic123@gmail.com>
2026-08-20 14:48:34 +0330

    script.js

--- script.js
+++ script.js
@@ -1,38 +1,72 @@
-const name="Artin";
-const age=14;
-const isLearning=true;
+const user = {
+    name: "Artin",
+    age: 14,
+    goal: "Full-Stack Developer",
+    isLearning: true,
+    skills: ["HTML", "CSS", "JavaScript"]
+};
 
-if (isLearning) {
-    console.log("I am Learning Full-Stack Development!")
-}else {
-    console.log(" I am not Learning right now")
+// Object
+console.log(user.goal);
+console.log(user.skills);
+
+// Conditions
+if (user.isLearning) {
+    console.log("I am learning Full-Stack Development!");
+} else {
+    console.log("I am not learning right now");
 }
 
-if (age === 14) {
-    console.log("I am 14 years old")    
-}else {
-    console.log("My age is different")
+if (user.age === 14) {
+    console.log("I am 14 years old");
+} else {
+    console.log("My age is different");
 }
 
-if (age >= 13 && isLearning) {
-    console.log("I am ready to become a Developer!")
+if (user.age >= 13 && user.isLearning) {
+    console.log("I am ready to become a Developer!");
 }
 
-function introduce(name, goal){
-    console.log(`Hi, my name is ${name} and I want to become a ${goal}!`)
+// Function
+function introduce(name, goal) {
+    console.log(`Hi, my name is ${name} and I want to become a ${goal}!`);
 }
 
+introduce(user.name, user.goal);
+
+// Function + return
 function multiply(a, b) {
-    return a*b
+    return a * b;
 }
 
-const result=multiply(5, 4);
-console.log(result)
+const result = multiply(5, 4);
+console.log(result);
 
-introduce("Artin", "Full-Stack Developer")
+// Array
+console.log(user.skills.length);
 
-const skills=["HTML","CSS","JavaScript"];
+user.skills.push("Python");
 
-console.log(skills)
+console.log(user.skills.length);
+console.log(user.skills);
 
-console.log(name,age,isLearning);
+// forEach
+user.skills.forEach(function(skill) {
+    console.log(`My skill is ${skill}`);
+});
+
+// DOM
+const title = document.getElementById("title");
+const button = document.getElementById("myButton");
+
+// Change text
+title.textContent = "I am becoming a Full-Stack Developer!";
+
+//
+let isStyled = false;
+
+// Button click
+button.addEventListener("click", function() {
+    title.style.fontFamily = "Arial";
+    title.style.color = "lime"
+});
